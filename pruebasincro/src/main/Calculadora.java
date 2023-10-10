3c226a08afca04acab59b3d8c121a7b50db3a3e7
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-10 19:03:07 +0200

    mi segundo token

--- pruebasincro/src/main/Calculadora.java
+++ pruebasincro/src/main/Calculadora.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1, numero2, resultado;
+        double num1, num2, resultado;
         char operador;
 
         System.out.print("Ingrese el primer número: ");
@@ -14,24 +14,24 @@ public class Calculadora {
         operador = scanner.next().charAt(0);
 
         System.out.print("Ingrese el segundo número: ");
-        numero2 = scanner.nextDouble();
+        num2 = scanner.nextDouble();
 
         switch (operador) {
             case '+':
-                resultado = num1 + numero2;
+                resultado = num1 + num2;
                 System.out.println("Resultado: " + resultado);
                 break;
             case '-':
-                resultado = num1 - numero2;
+                resultado = num1 - num2;
                 System.out.println("Resultado: " + resultado);
                 break;
             case '*':
-                resultado = num1 * numero2;
+                resultado = num1 * num2;
                 System.out.println("Resultado: " + resultado);
                 break;
             case '/':
-                if (numero2 != 0) {
-                    resultado = num1 / numero2;
+                if (num2 != 0) {
+                    resultado = num1 / num2;
                     System.out.println("Resultado: " + resultado);
                 } else {
                     System.out.println("Error: División por cero no permitida.");
